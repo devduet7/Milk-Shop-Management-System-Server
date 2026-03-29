513ce3bf7f871b7514e30919ab617acb731265e4
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-03-29 11:53:15 +0500

    Add corsOptions.js for CORS Configuration

---
+++ config/corsOptions.js
@@ -0,0 +1,24 @@
+// <== IMPORTS ==>
+import allowedOrigins from "./allowedOrigins.js";
+
+// <== CORS OPTIONS ==>
+const corsOptions = {
+  // ALLOWED ORIGINS
+  origin: (origin, callback) => {
+    // CHECKING IF THE ORIGIN IS ALLOWED
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      // ALLOWING THE ORIGIN
+      callback(null, true);
+    } else {
+      // DENYING THE ORIGIN
+      callback(new Error("Not Allowed by CORS"));
+    }
+  },
+  // ALLOWING CREDENTIALS
+  credentials: true,
+  // ALLOWING OPTIONS
+  optionsSuccessStatus: 200,
+};
+
+// <== EXPORTING CORS OPTIONS ==>
+export default corsOptions;
