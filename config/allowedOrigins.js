8ced0e2e4cf0e3b05e7c81d3e3f3424c1cac99cc
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-03-29 11:53:09 +0500

    Add allowedOrigins.js for CORS Configuration

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+// <== ALLOWED ORIGINS FOR CORS ==>
+const allowedOrigins = [
+  // FRONTEND LOCALHOST
+  "http://localhost:8080",
+];
+
+// <== EXPORTING ALLOWED ORIGINS ==>
+export default allowedOrigins;
