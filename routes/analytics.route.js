02ea3d6de667b7afe0fa6a10885991dca5acc742
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-06-24 11:58:41 +0500

    Add Permission Check to Analytics Route for Enhanced Access Control

--- routes/analytics.route.js
+++ routes/analytics.route.js
@@ -1,6 +1,7 @@
 // <== IMPORTS ==>
 import express from "express";
 import isAuthenticated from "../middleware/isAuthenticated.js";
+import { requirePermission } from "../middleware/authorize.js";
 import { getAnalyticsData } from "../controllers/analytics.controller.js";
 import { validateAnalyticsQuery } from "../validators/analytics.validator.js";
 
@@ -12,7 +13,12 @@ router.use(isAuthenticated);
 
 // <== ROUTES ==>
 // GET COMPREHENSIVE ANALYTICS CHART DATA FOR THE SELECTED MONTH
-router.get("/", validateAnalyticsQuery, getAnalyticsData);
+router.get(
+  "/",
+  requirePermission("analytics", "read"),
+  validateAnalyticsQuery,
+  getAnalyticsData,
+);
 
 // <== EXPORTING ROUTER ==>
 export default router;
