d33a4159719a517a26601ce671d6fe519b5c0511
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-08-11 12:37:04 +0500

    Refactor Cloudinary Configuration to Use Config Module for Environment Variables

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,11 +1,12 @@
 // <== IMPORTS ==>
+import { env } from "../config/env.js";
 import { v2 as cloudinary } from "cloudinary";
 
 // <== CONFIGURING CLOUDINARY ==>
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  cloud_name: env.CLOUD_NAME,
+  api_key: env.API_KEY,
+  api_secret: env.API_SECRET,
 });
 
 // <== EXPORTING CLOUDINARY ==>
